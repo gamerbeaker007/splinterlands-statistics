7cb747a35adcd2f6f4cc8b5bf838231b3b9bd58c
gamerbeaker <gamerbeaker@gmail.com>
2023-05-07 21:09:01 +0200

    WIP

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 
+from numpy.matlib import ones
+
 from src.api import spl
 import pandas as pd
 
@@ -23,16 +25,43 @@ def update_collection(collections_df, winner_uids, winner):
 
 
 def main():
+    extra_columns = ['wild_win', 'wild_loss', 'modern_win', 'modern_loss', 'total_battles']
     collections_df = pd.DataFrame()
     battle_history = pd.DataFrame()
     for account in config.account_names:
         collections_df = pd.concat([collections_df, spl.get_player_collection_df(account)])
         battle_history = pd.concat([battle_history, spl.get_battle_history_df(account)])
 
-
     bas_dir = os.path.join(os.getcwd(), config.store_dir)
     file = os.path.join(bas_dir, 'collection.csv')
-    collections_df.to_csv(file)
+    if os.path.isfile(file):
+        old_collection = pd.read_csv(file, index_col=[0]).sort_index()
+
+        # Update to new collection from spl
+        extra_columns_with_uid = extra_columns.copy()
+        extra_columns_with_uid.append('uid')
+        old_collection = collections_df.reset_index()\
+            .merge(old_collection.reset_index()[extra_columns_with_uid], on='uid', how='left')\
+            .set_index('uid')
+        old_collection.fillna(0, inplace=True)
+        old_collection.sort_index().to_csv(file)
+    else:
+        collections_df[extra_columns] = 0
+        collections_df.sort_index().to_csv(file)
+
+    # combined
+    # card | wild_win | wild_loss | modern_win | modern_loss
+
+    # wild
+    # card |win | loss
+    # modern
+    # card | win | loss
+
+    # rulesets
+    # ruleset       | card   | win  | loss
+    # weak_magic    | card 1 | 1    | 0
+    # weak_magic    | card 2 | 2    | 6
+
 
     # TODO Filter out already process battles
 
