fa46278ac7c2553f03bbc24c5d52a4d75d42c56d
rb83421 <r.boerwinkel@gmail.com>
2023-08-04 18:09:03 +0200

    Add edition graph

--- src/graphs/portfolio_graph.py
+++ src/graphs/portfolio_graph.py
@@ -1,4 +1,8 @@
+import plotly.colors as pc
 import plotly.express as px
+import plotly.graph_objects as go
+
+from src.static.static_values_enum import Edition
 
 
 def plot_portfolio_total(portfolio_df, combined_users, theme):
@@ -47,4 +51,68 @@ def plot_portfolio_all(df, theme, skip_zero=True):
         ),
 
     )
+    return fig
+
+
+def get_editions_fig(editions_df, theme):
+    fig = go.Figure()
+    max_value = editions_df.loc[:, editions_df.columns.str.endswith("_market_value")].max().max()
+    max_bcx = editions_df.loc[:, editions_df.columns.str.endswith("_bcx")].max().max()
+
+    # Get a discrete color palette from Plotly
+    color_palette = pc.qualitative.Plotly
+    color_index = 0  # Initialize the index for cycling colors
+
+    for edition in Edition.list_names():
+        if str(edition) + "_bcx" in editions_df.columns.tolist():
+            legend_group = edition
+            current_color = color_palette[color_index]
+
+            market_value_trace = go.Scatter(x=editions_df.index,
+                                            y=editions_df[str(edition) + '_market_value'],
+                                            mode='lines',
+                                            legendgroup=legend_group,
+                                            line=dict(color=current_color),  # Set line color
+                                            name=str(edition))
+            fig.add_trace(market_value_trace)
+            bcx__trace = go.Scatter(x=editions_df.index,
+                                    y=editions_df[str(edition) + '_bcx'],
+                                    mode='lines',
+                                    legendgroup=legend_group,
+                                    showlegend=False,
+                                    line=dict(color=current_color, dash='dash'),
+                                    name=str(edition) + ' bcx',
+                                    yaxis='y2')
+            fig.add_trace(bcx__trace)
+            color_index = (color_index + 1) % len(color_palette)  # Move to the next color
+
+    fig.update_layout(
+        template=theme,
+        title_text="Editions",
+        hovermode="x unified",
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ),
+        xaxis=dict(
+            title='date'
+        ),
+
+        yaxis2=dict(
+            overlaying='y',
+            side='right',
+            position=1.0,  # Adjust the position of the secondary y-axis
+            range=[0, max_bcx * 1.05],
+            title='bcx',
+        ),
+        yaxis1=dict(
+            showgrid=False,
+            range=[0, max_value * 1.05],
+            title="value $",
+        ),
+    )
+
     return fig
--- src/pages/portfolio_pages/portfolio_editions.py
+++ src/pages/portfolio_pages/portfolio_editions.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from aio import ThemeSwitchAIO
-from dash import Output, Input, html, dcc
+from dash import Output, Input, dcc
 
 from src.configuration import config
+from src.graphs import portfolio_graph
 from src.pages.main_dash import app
+from src.static.static_values_enum import Edition
 from src.utils import chart_util
-import dash_bootstrap_components as dbc
 
 
 def get_edition_layout():
@@ -23,10 +24,27 @@ def update_portfolio_editions_graph(filtered_df, toggle):
     if not filtered_df:
         return chart_util.blank_fig(theme)
     else:
-        temp_df = pd.read_json(filtered_df, orient='split')
+        portfolio_df = pd.read_json(filtered_df, orient='split')
 
-    if temp_df.empty:
+    if portfolio_df.empty:
         return chart_util.blank_fig(theme)
     else:
-        # TODO
-        return chart_util.blank_fig(theme)
+        portfolio_df.sort_values(by='date', inplace=True)
+        editions_df = portfolio_df.loc[:,
+                      portfolio_df.columns.str.startswith('date') |
+                      (portfolio_df.columns.str.startswith(tuple(Edition.list_names())) &
+                       (portfolio_df.columns.str.endswith("market_value") | portfolio_df.columns.str.endswith(
+                           "_bcx"))
+                       )]
+
+        # drop empty rows
+        editions_df = editions_df.set_index('date').dropna(how='all')
+
+        # drop columns that have a sum of 0 bxc and market_value
+        for edition in Edition.list_names():
+            if (editions_df[str(edition) + "_market_value"] + editions_df[str(edition) + "_bcx"]).sum() == 0:
+                # drop columns that have a sum of 0
+                editions_df.drop(columns=str(edition) + "_market_value", inplace=True)
+                editions_df.drop(columns=str(edition) + "_bcx", inplace=True)
+
+        return portfolio_graph.get_editions_fig(editions_df, theme)
