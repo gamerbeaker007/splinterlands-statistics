42cb60f513f44d740b8d75fa5bea7e36a1a40202
rb83421 <r.boerwinkel@gmail.com>
2023-12-29 10:35:19 +0100

    fix typo

--- src/pages/card_pages/card_page_ruleset.py
+++ src/pages/card_pages/card_page_ruleset.py
@@ -34,12 +34,12 @@ def update_top_cards(filtered_df):
         if not top_5.empty:
             result_layout.append(html.H6("Most played with ruleset top 5"))
             for index, value in top_5.items():
-                result_layout.append(html.P(str(index) + ": " + str(value), style={'marginBottom': '5px'}))
+                result_layout.append(html.P(str(index) + " (" + str(value) + ")", style={'marginBottom': '5px'}))
 
         match_type = filtered_df.match_type.value_counts()
         if not match_type.empty:
             result_layout.append(html.H6("Match type used", style={'marginTop': '20px'}))
             for index, value in match_type.items():
-                result_layout.append(html.P(str(index) + ": " + str(value), style={'marginBottom': '5px'}))
+                result_layout.append(html.P(str(index) + " (" + str(value) + ")", style={'marginBottom': '5px'}))
 
     return html.Div(result_layout)
